f789c28beb2150ed89317f7d80bdc59314e735c7
shlheidari <soheilheidari289@yahoo.com>
2024-08-12 17:36:58 +0330

    add html

--- myworld/myworld/settings.py
+++ myworld/myworld/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movieranking.apps.MovierankingConfig'
 ]
 
 MIDDLEWARE = [
